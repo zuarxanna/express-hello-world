c12ac92cd533ad384b4e4123be9d41d703283a49
Muhammad Ikhsan <nikhsan417@gmail.com>
2023-05-15 21:47:21 +0700

    add port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen("3000", () => {
-  console.log(`app run on port 3000`);
+const port = process.en.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app run on port ${port}`);
 });
